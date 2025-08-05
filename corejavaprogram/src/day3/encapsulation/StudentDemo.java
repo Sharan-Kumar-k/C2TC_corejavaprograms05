503c285525ce18b98e9a6644dabff6dca5814fd2
SHARANKUMAR <SHARANKUMAR@LAPTOP-6H1IGBG0>
2025-08-05 22:55:53 +0530

    new changes done

---
+++ corejavaprogram/src/day3/encapsulation/StudentDemo.java
@@ -0,0 +1,22 @@
+package day3.encapsulation;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setSid(1);
+		s.setSname("sharan");
+		s.setAvg(89.5F);
+		System.out.println(s);
+		Student S = new Student();
+		S.setSid(2);
+		S.setSname("sharan kumar");
+		S.setAvg(90.5F);
+		System.out.println(S);
+		System.out.println("Student id:"+s.getSid());
+		System.out.println("Student name:"+s.getSname());
+		System.out.println("student avg"+s.getAvg());
+
+	}
+
+}
